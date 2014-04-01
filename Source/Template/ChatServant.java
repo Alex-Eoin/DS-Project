311bc175089808db8f5b957b95b4646130523991
AJQShake <quigley.alex@gmail.com>
2014-04-01 15:51:05 +0100

    updated server

--- Source/Template/ChatServant.java
+++ Source/Template/ChatServant.java
@@ -4,11 +4,15 @@ class ChatServant implements ChatRoomOperations
 {
 	Member[] members ;
 	String[] names ;
+	String[] currentGame ; 
+	
 	int numberMembers ;
 
 	ChatServant() {
-		members = new Member[10] ;
-		names = new String[10] ;
+		members = new Member[10];
+		names = new String[10];
+		currentGame = { "empty", "empty", "data", };
+		
 		numberMembers = 0 ;
 	}
 
@@ -25,6 +29,23 @@ class ChatServant implements ChatRoomOperations
 
 	}
 
+	public void registerGame(Member m, String name) {
+		members[numberMembers] = m ;
+		names[numberMembers] = name ;
+		
+		for (int i=0; i<numberMembers; i++) {
+			members[i].callBack("New Member: " + name) ;
+		}
+		numberMembers++ ;
+		m.callBack("Thank you " + name + ", you are now registered");
+		System.out.println(name + " has just joined the chat room.");
+
+	}
+	
+	public void exitGame(){
+		currentGame
+	}
+	
 	public void chat(String c, String name) {
 		System.out.println(name + " said: " + c);
 		String s = "Message from " + name + ": " + c  ;
@@ -32,4 +53,6 @@ class ChatServant implements ChatRoomOperations
 			members[i].callBack(s) ;
 		}
 	}
+	
+	
 }
--- Source/Template/ChatServer.java
+++ Source/Template/ChatServer.java
@@ -1,5 +1,5 @@
 // ChatServer.java
-import  Chat.* ;
+import Chat.* ;
 import org.omg.CORBA.*;
 import java.io.* ;
 import org.omg.CosNaming.*;
@@ -9,25 +9,25 @@ import org.omg.CosNaming.NamingContextPackage.*;
 public class ChatServer {
 
     public static void main(String args[]) {
-	try{
-	    	// create and initialize the ORB
-	    	ORB orb = ORB.init(args, null);
-
-			ChatRoom c = new ChatRoom_Tie(new ChatServant()) ;
+		/** Declarations */
+		ORB orb;
+		ChatRoom c;
+		org.omg.CORBA.Object nameObj;
+		NameComponent[] name = new NameComponent[1];
+		NamingContext rootCtx;
+		
+		try{
+			
+	    	orb = ORB.init(args, null);
+			c = new ChatRoom_Tie(new ChatServant()) ;
 	    	orb.connect(c);
 
-	    	//Create  Object reference from the NameService
-			org.omg.CORBA.Object nameObj=orb.resolve_initial_references("NameService");
-			NamingContext rootCtx=NamingContextHelper.narrow(nameObj);
-			NameComponent[] name = new NameComponent[1];
+			nameObj = orb.resolve_initial_references("NameService");
+			rootCtx=NamingContextHelper.narrow(nameObj);
 			name[0] = new NameComponent("Chatroom", "Object");
 			rootCtx.rebind(name,c);
-/*
-	    	ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("IOR")) ;
-	    	out.writeObject(orb.object_to_string(c)) ;
-			out.close() ;
-*/
-			System.out.println("The Chat Room is now open for gossip ...") ;
+
+			System.out.println("The Game Server is now running ...") ;
 			orb.run();
 
 		} catch (Exception e) {
@@ -35,4 +35,6 @@ public class ChatServer {
 	    		e.printStackTrace(System.out);
 		}
     }
+	
+	
 }
